2f3cd0034b3d834f2e54ba6530f44ab2661545b5
Tixieej <rixtdieuwke@live.nl>
2020-12-04 23:22:09 +0100

    aoc4 working version

--- aoc4/main.py
+++ aoc4/main.py
@@ -8,32 +8,24 @@ def exercise2(Passports):
                     if "ecl" in info and info["ecl"] in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
                         if "pid" in info and len(info["pid"]) == 9:
                             if "hcl" in info and info["hcl"][:1] == '#':
-                                print(info["hcl"])
                                 if "hgt" in info and "cm" in info["hgt"]:
                                     if (info["hgt"][:1] == '1') and (150 <= int(info["hgt"][:3])) and (int(info["hgt"][:3]) <= 193):
                                         valid = valid + 1
                                 elif "hgt" in info and "in" in info["hgt"]:
                                     if (59 <= int(info["hgt"][:2])) and (int(info["hgt"][:2]) <= 76):
                                         valid = valid + 1
-    print(valid)
+    return(valid)
 
 def exercise1(Passports):
     valid = 0
     for passport in Passports:
         info = passport.split()
-        if len(info) < 8:
-            if "cid" in passport or len(info) < 7:
-                print("INVALID:")
-            else:
-                valid = valid + 1
-                print("VALID:")
-        else:
+        if len(info) == 8 or (len(info) == 7 and "cid" not in passport):
             valid = valid + 1
-            print("VALID:")
-    print(valid)
+    return(valid)
 
 #### Main ####
 Passports = open('input', 'r').read().split("\n\n")
-print(len(Passports))
-exercise1(Passports)
-exercise2(Passports)
+print("There are", len(Passports), "passports total.")
+print("Valid passports exercise 1:", exercise1(Passports))
+print("Valid passports exercise 2:", exercise2(Passports))
